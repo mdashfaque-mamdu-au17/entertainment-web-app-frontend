f42a68398f92736362ef692e8f93fc8c71480077
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-03-16 15:27:47 +0530

    colors added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-red': '#fc4747',
+        'primary-black': '#10141e',
+        'greyish-blue': '#5a698f',
+        'semi-dark-blue': '#161d2f',
+        'primary-white': '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 };
