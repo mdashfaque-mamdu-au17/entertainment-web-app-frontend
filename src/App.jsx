fc880fc77f72f11af7ee4445c7c55ca77348907e
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-03-20 23:22:22 +0530

    login page testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
-import Button from './components/Button';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div>
-        <Button>Create an account</Button>
-      </div>
+      <Login />
     </div>
   );
 }
