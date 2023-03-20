b2ad38aa5bd270786884cb4d0657c834f3cab6dd
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-03-20 23:22:42 +0530

    imports updated

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,6 @@
 import Button from './Button';
-
-export { Button };
+import Input from './Input';
+import InputBoxWrapper from './InputBoxWrapper';
+import Heading from './Heading';
+import Title from './Title';
+export { Button, Input, InputBoxWrapper, Heading, Title };
