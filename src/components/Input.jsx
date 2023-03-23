ccf9a1a42b5c5ddea454a94a100c1fa68c6463a2
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-03-23 14:59:37 +0530

    showing the correct error message

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,17 +3,25 @@ import { useField } from 'formik';
 import classNames from 'classnames';
 
 const Input = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
   return (
     <div className="w-[279px] md:w-[336px] relative">
       <label htmlFor={props.id || props.name} hidden>
         {label}
       </label>
       <input
+        {...field}
         {...props}
         className={classNames(
-          'w-full focus:outline-none bg-inherit text-[15px] leading-[19px] font-light placeholder:text-primary-white/100/50 text-primary-white pb-[17px] border-b-[1px] border-b-primary-white/50 focus:border-b-primary-white pl-4'
+          'w-full focus:outline-none bg-inherit text-[15px] leading-[19px] font-light placeholder:text-primary-white/100/50 text-primary-white pb-[17px] border-b-[1px] border-b-primary-white/50 focus:border-b-primary-white pl-4',
+          meta.touched && meta.error && 'border-b-primary-red'
         )}
       />
+      {meta.touched && meta.error && (
+        <div className="text-[13px] leading-[13px] text-primary-red absolute bottom-[18px] right-4">
+          {meta.error}
+        </div>
+      )}
     </div>
   );
 };
