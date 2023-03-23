98c3cfb6919d8290566e808bb84d578498389aff
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-03-23 14:59:59 +0530

    max-w control for both mobile and desktop

--- src/components/InputBoxWrapper.jsx
+++ src/components/InputBoxWrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const InputBoxWrapper = ({ children }) => {
   return (
-    <section className="bg-semi-dark-blue rounded-[10px] md:rounded-[20px]">
+    <section className="bg-semi-dark-blue rounded-[10px] md:rounded-[20px] max-w-[327px] md:max-w-[400px] mx-auto">
       {children}
     </section>
   );
