aed587b14e31b58da46729785edf6b78279c0d95
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-06-22 22:17:19 +0530

    toast container changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Signup, Login, Bookmarked, Home, Movies, TvSeries } from './pages';
+import { ToastContainer } from 'react-toastify';
 
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
   { path: '/movies', element: <Movies /> },
   { path: '/tv-series', element: <TvSeries /> },
-  { path: 'bookmarks', element: <Bookmarked /> },
+  { path: '/bookmarks', element: <Bookmarked /> },
   { path: '/login', element: <Login /> },
   { path: '/signup', element: <Signup /> },
 ]);
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer position="top-center" />
+    </>
+  );
 }
 
 export default App;
