97732ecbed894c5d95c63875a31dd6d5a4acf387
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2023-07-11 23:13:06 +0530

    api integration completed for movies page

--- src/components/AllMoviesSection.jsx
+++ src/components/AllMoviesSection.jsx
@@ -7,7 +7,7 @@ const AllMoviesSection = ({ isLoading, data, isError, error }) => {
       {!isError && <TitleHeading>Recommended for you</TitleHeading>}
       {isLoading && <SingleMoviesLoader />}
       {!isLoading && !isError && data?.movies?.length > 0 && (
-        <div className="mt-6 flex flex-wrap gap-x-[15px] gap-y-4 sm:gap-x-[30px] sm:gap-y-6 lg:mt-8 lg:gapx-10 lg:gap-y-8">
+        <div className="mt-6 flex flex-wrap gap-x-[15px] gap-y-4 sm:gap-x-[29px] sm:gap-y-6 lg:mt-8 lg:gap-x-10 lg:gap-y-8">
           {data?.movies?.map((movie) => {
             return <SingleMovie key={movie?._id} {...movie} />;
           })}
--- src/pages/dashboard/Movies.jsx
+++ src/pages/dashboard/Movies.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useGlobalContext } from '../../context';
 import { useNavigate } from 'react-router-dom';
 import RootLayout from './RootLayout';
-import { MoviesSection, SearchInput } from '../../components';
+import { MoviesSection, SearchInput, SearchResults } from '../../components';
+import { useQuery } from '@tanstack/react-query';
+import customFetch from '../../utils/axios';
 
 const Movies = () => {
   const [searchTerm, setSearchTerm] = useState();
@@ -19,6 +21,22 @@ const Movies = () => {
     }
   }, [user]);
 
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ['movies'],
+    queryFn: async () => {
+      const { data } = await customFetch.get('/movies?category=Movie');
+      return data;
+    },
+  });
+
+  const searchResults =
+    searchTerm &&
+    data?.movies?.filter((movie) => {
+      return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
+  console.log(searchTerm, 'search term');
+
   return (
     <RootLayout>
       <div className="mt-6 px-4 sm:mt-[33px] sm:px-[25px] lg:mt-16 lg:px-0 lg:ml-[128px] lg:px-8 z-20">
@@ -34,9 +52,21 @@ const Movies = () => {
       </div>
 
       {/* movies */}
-      <div className="mt-6 px-4 sm:mt-[33px] sm:px-[25px] lg:mt-[34px] lg:px-0 lg:ml-[128px] lg:px-8 z-20">
-        <MoviesSection />
-      </div>
+      {!searchTerm && (
+        <div className="mt-6 px-4 sm:mt-[33px] sm:px-[25px] lg:mt-[34px] lg:px-0 lg:ml-[128px] lg:px-8 z-20">
+          <MoviesSection
+            isLoading={isLoading}
+            data={data}
+            isError={isError}
+            error={error}
+          />
+        </div>
+      )}
+
+      {/* search results */}
+      {searchTerm && (
+        <SearchResults searchTerm={searchTerm} searchResults={searchResults} />
+      )}
     </RootLayout>
   );
 };
